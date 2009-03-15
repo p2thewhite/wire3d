98ba2312508646dae26ac15b0e30491041235637
antibyte <antibyte@7d1a0020-0be9-11de-8884-7b8501395459>
2009-03-15 14:23:45 +0000

    * worked on Transformation class

--- trunk/Wire/Engine/WireTransformation.h
+++ trunk/Wire/Engine/WireTransformation.h
@@ -2,6 +2,7 @@
 #ifndef WIRETRANSFORMATION_H
 #define WIRETRANSFORMATION_H
 
+#include "../Foundation/WireTypes.h"
 #include "../Foundation/WireMatrix34.h"
 
 namespace Wire
@@ -13,9 +14,34 @@ public:
 	Transformation();
  	~Transformation();
 
+	// Hints about the structure of the transformation.  In the common case
+	// of M = R*S, IsRSMatrix() returns true.
+	Bool IsIdentity () const;
+	Bool IsRSMatrix () const;
+	Bool IsUniformScale () const;
+
+	inline operator Matrix34f ();
+ 	inline operator const Matrix34f () const;
+
+	void SetMatrix(const Matrix34f& rMatrix);
+	const Matrix34f& GetMatrix() const;
+	void SetTranslate(const Vector3f& rTranslate);
+	Vector3f GetTranslate() const;
+	void SetScale(const Vector3f& rScale);
+	const Vector3f& GetScale() const;
+	void SetUniformScale (Float scale);
+	Float GetUniformScale() const;
+
+	// Pack the transformation into a 3-by-4 matrix with implicit 4th row.
+	void GetTransformation(Matrix34f& rMatrix) const;
+
 private:
-	Matrix34f mTransformation;
+	Matrix34f mMatrix;
 	Vector3f mScale;
+
+	Bool mIsIdentity;
+	Bool mIsRSMatrix;
+	Bool mIsUniformScale;
 };
 
 #include "WireTransformation.inl"
--- trunk/Wire/Foundation/WireVector3.cpp
+++ trunk/Wire/Foundation/WireVector3.cpp
@@ -2,14 +2,14 @@
 
 using namespace Wire;
 
-template<> const Vector3<float> Vector3<float>::ZERO(0.0f, 0.0f, 0.0f);
-template<> const Vector3<float> Vector3<float>::UNIT_X(1.0f, 0.0f, 0.0f);
-template<> const Vector3<float> Vector3<float>::UNIT_Y(0.0f, 1.0f, 0.0f);
-template<> const Vector3<float> Vector3<float>::UNIT_Z(0.0f, 0.0f, 1.0f);
-template<> const Vector3<float> Vector3<float>::ONE(1.0f, 1.0f, 1.0f);
+template<> const Vector3<Float> Vector3<Float>::ZERO(0.0f, 0.0f, 0.0f);
+template<> const Vector3<Float> Vector3<Float>::UNIT_X(1.0f, 0.0f, 0.0f);
+template<> const Vector3<Float> Vector3<Float>::UNIT_Y(0.0f, 1.0f, 0.0f);
+template<> const Vector3<Float> Vector3<Float>::UNIT_Z(0.0f, 0.0f, 1.0f);
+template<> const Vector3<Float> Vector3<Float>::ONE(1.0f, 1.0f, 1.0f);
 
-template<> const Vector3<double> Vector3<double>::ZERO(0.0, 0.0, 0.0);
-template<> const Vector3<double> Vector3<double>::UNIT_X(1.0, 0.0, 0.0);
-template<> const Vector3<double> Vector3<double>::UNIT_Y(0.0, 1.0, 0.0);
-template<> const Vector3<double> Vector3<double>::UNIT_Z(0.0, 0.0, 1.0);
-template<> const Vector3<double> Vector3<double>::ONE(1.0, 1.0, 1.0);
+template<> const Vector3<Double> Vector3<Double>::ZERO(0.0, 0.0, 0.0);
+template<> const Vector3<Double> Vector3<Double>::UNIT_X(1.0, 0.0, 0.0);
+template<> const Vector3<Double> Vector3<Double>::UNIT_Y(0.0, 1.0, 0.0);
+template<> const Vector3<Double> Vector3<Double>::UNIT_Z(0.0, 0.0, 1.0);
+template<> const Vector3<Double> Vector3<Double>::ONE(1.0, 1.0, 1.0);
