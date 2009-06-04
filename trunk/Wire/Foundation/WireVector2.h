bbac8e28464717eda5666d997cedc166273001d0
antibyte <antibyte@7d1a0020-0be9-11de-8884-7b8501395459>
2009-06-04 20:05:36 +0000

    * added Vector2 * using CONF_GetAspectRatio * removed Intellisense absolute paths in projects files (Visual Assist does the job for me anyways)

---
+++ trunk/Wire/Foundation/WireVector2.h
@@ -0,0 +1,47 @@
+#pragma once
+#ifndef WIREVECTOR2_H
+#define WIREVECTOR2_H
+
+#include "WireTypes.h"
+
+namespace Wire
+{
+
+template <class Real>
+class Vector2
+{
+public:
+	// construction
+	Vector2();  // uninitialized
+	Vector2(Real x, Real y);
+
+	// coordinate access
+	inline operator Real* ();
+	inline operator const Real* () const;
+	inline Real X() const;
+	inline Real X();
+	inline Real Y() const;
+	inline Real Y();
+
+	// arithmetic operations
+	inline Vector2 operator+ (const Vector2& rVector);
+	inline Vector2 operator- (const Vector2& rVector);
+	inline Vector2 operator* (Real scalar);
+	inline Vector2 operator/ (Real scalar);
+	inline Vector2 operator- () const;
+
+	// vector operations
+	inline Real Length() const;
+	inline Real SquaredLength() const;
+
+private:
+	Real mTuple[2];
+};
+
+#include "WireVector2.inl"
+
+typedef Vector2<Float> Vector2f;
+
+}
+
+#endif /* WIREVECTOR2_H */
