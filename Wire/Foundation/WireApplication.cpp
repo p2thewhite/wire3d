70fdb32c6bfb99bf268bace44cb568a0a4179818
antibyte <antibyte@7d1a0020-0be9-11de-8884-7b8501395459>
2009-10-13 17:26:50 +0000

    * Dx9 Application and Renderer in progress

--- Wire/Foundation/WireApplication.cpp
+++ Wire/Foundation/WireApplication.cpp
@@ -37,6 +37,12 @@ Application* Application::GetApplication()
 }
 
 //----------------------------------------------------------------------------
+Renderer* Application::GetRenderer() const
+{
+	return mpRenderer;
+}
+
+//----------------------------------------------------------------------------
 Int main(Int argc, Char** argv)
 {
 	Main::Initialize();
