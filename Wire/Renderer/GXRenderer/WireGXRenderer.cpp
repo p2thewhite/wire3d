13e82fc9278c33cf3ae31ae35e91dd1222054f3b
antibyte <antibyte@7d1a0020-0be9-11de-8884-7b8501395459>
2009-11-29 11:25:39 +0000

    * preparing 32byte aligned buffers for GXSetArray

--- Wire/Renderer/GXRenderer/WireGXRenderer.cpp
+++ Wire/Renderer/GXRenderer/WireGXRenderer.cpp
@@ -187,38 +187,35 @@ void GXRenderer::DrawElements()
 {
 	mIsFrameBufferDirty = true;
 
+	Matrix34F model;
+	mpGeometry->World.GetTransformation(model);
+	// load the modelview matrix into matrix memory
+	GXLoadPosMtxImm(model * mViewMatrix, GX_PNMTX0);
+
+	const VertexBuffer* pVBuffer = mpGeometry->VBuffer;
+	const ResourceIdentifier* pID = pVBuffer->GetIdentifier(0, this);
+	WIRE_ASSERT(pID);
+	const VBufferID* pResource = static_cast<const VBufferID*>(pID);
+
 	// setup the vertex descriptor
 	// tells the flipper to expect direct data
 	GXClearVtxDesc();
-	GXSetVtxDesc(GX_VA_POS, GX_DIRECT);
-	GXSetVtxDesc(GX_VA_CLR0, GX_DIRECT);
+	const TArray<VBufferID::VertexElement>& rElements = *(pResource->Elements);
+	for (UInt i = 0; i < rElements.GetQuantity(); i++)
+	{
+		GXSetVtxDesc(rElements[i].Attr, GX_DIRECT);
+		GXSetVtxAttrFmt(GX_VTXFMT0, rElements[i].Attr, rElements[i].CompCnt,
+			rElements[i].CompType, 0);
+	}
 
 	GXSetCullMode(GX_CULL_BACK);
-
-	// setup the vertex attribute table
-	// describes the data
-	// args: vat location 0-7, type of data, data format, size, scale
-	// so for ex. in the first call we are sending position data with
-	// 3 values X,Y,Z of size F32. scale sets the number of fractional
-	// bits for non float data.
-	GXSetVtxAttrFmt(GX_VTXFMT0, GX_VA_POS, GX_POS_XYZ, GX_F32, 0);
-	GXSetVtxAttrFmt(GX_VTXFMT0, GX_VA_CLR0, GX_CLR_RGBA, GX_RGBA8, 0);
 
 	GXSetNumChans(1);
 	GXSetNumTexGens(0);
 	GXSetTevOrder(GX_TEVSTAGE0, GX_TEXCOORD_NULL, GX_TEXMAP_NULL, GX_COLOR0A0);
 	GXSetTevOp(GX_TEVSTAGE0, GX_PASSCLR);
 
-	Matrix34F model;
-	mpGeometry->World.GetTransformation(model);
-	// load the modelview matrix into matrix memory
-	GXLoadPosMtxImm(model * mViewMatrix, GX_PNMTX0);
-
 	const IndexBuffer* pIBuffer = mpGeometry->IBuffer;
-	const VertexBuffer* pVBuffer = mpGeometry->VBuffer;
-	const ResourceIdentifier* pID = pVBuffer->GetIdentifier(0, this);
-	WIRE_ASSERT(pID);
-	const VBufferID* pResource = static_cast<const VBufferID*>(pID);
 	UChar* pVBData = reinterpret_cast<UChar*>(pResource->ID);
 
 	GXBegin(GX_TRIANGLES, GX_VTXFMT0, pIBuffer->GetIndexQuantity());
