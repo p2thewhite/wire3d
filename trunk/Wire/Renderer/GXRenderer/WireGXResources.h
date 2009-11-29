d7e93c0dae2e261878276617f8e6ffdf8e6ea444
antibyte <antibyte@7d1a0020-0be9-11de-8884-7b8501395459>
2009-11-29 11:25:39 +0000

    * preparing 32byte aligned buffers for GXSetArray

--- trunk/Wire/Renderer/GXRenderer/WireGXResources.h
+++ trunk/Wire/Renderer/GXRenderer/WireGXResources.h
@@ -10,8 +10,17 @@ namespace Wire
 class /*WIRE_RENDERER_ITEM*/ VBufferID : public ResourceIdentifier
 {
 public:
+	struct VertexElement
+	{
+		void* Data;
+		UChar Attr;
+		UChar CompCnt;
+		UChar CompType;
+	};
+
 	Float* ID;
 	UInt VertexSize;
+	TArray<VertexElement>* Elements;
 };
 
 }
--- trunk/Wire/Renderer/GXRenderer/WireGXResourcesLR.cpp
+++ trunk/Wire/Renderer/GXRenderer/WireGXResourcesLR.cpp
@@ -16,10 +16,21 @@ void GXRenderer::OnLoadVBuffer(ResourceIdentifier*& rID,
 	UInt channels = 0;
 	const VertexAttributes& rIAttr = pVBuffer->GetAttributes();
 
+	pResource->Elements = WIRE_NEW TArray<VBufferID::VertexElement>(2);
+
+	VBufferID::VertexElement element;
+
 	if (rIAttr.GetPositionChannels() > 0)
 	{
 		channels = rIAttr.GetPositionChannels();
 		vertexSize += channels * sizeof(Float);
+
+ 		element.Data = memalign(32, channels * sizeof(Float) * pVBuffer->
+ 			GetVertexQuantity());
+ 		element.Attr = GX_VA_POS;
+ 		element.CompCnt = GX_POS_XYZ;
+ 		element.CompType = GX_F32;
+ 		pResource->Elements->Append(element);
 	}
 
 	for (UInt unit = 0; unit < rIAttr.GetColorChannelQuantity(); unit++)
@@ -27,6 +38,13 @@ void GXRenderer::OnLoadVBuffer(ResourceIdentifier*& rID,
 		if (rIAttr.GetColorChannels(unit) > 0)
 		{
 			vertexSize += sizeof(UInt);
+	
+ 			element.Data = memalign(32, sizeof(UInt) * pVBuffer->
+ 				GetVertexQuantity());
+ 			element.Attr = GX_VA_CLR0;
+ 			element.CompCnt = GX_CLR_RGBA;
+ 			element.CompType = GX_RGBA8;
+ 			pResource->Elements->Append(element);
 		}
 	}
 
@@ -86,6 +104,13 @@ void GXRenderer::Convert(const VertexBuffer* pSrc, Float* pDst)
 void GXRenderer::OnReleaseVBuffer(ResourceIdentifier* pID)
 {
 	VBufferID* pResource = static_cast<VBufferID*>(pID);
-	free(pResource->ID);		// allocated using memalign, not using new
+	TArray<VBufferID::VertexElement>& rElements = *(pResource->Elements);
+	for (UInt i = 0; i < rElements.GetQuantity(); i++)
+	{
+		free(rElements[i].Data);	// allocated using memalign, not using new
+	}
+
+	WIRE_DELETE pResource->Elements;
+	free(pResource->ID);			// allocated using memalign, not using new
 	WIRE_DELETE pResource;
 }
