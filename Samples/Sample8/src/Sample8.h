a90eb6ea9d153067ae16a93585f3b92850549f12
antibyte <antibyte@7d1a0020-0be9-11de-8884-7b8501395459>
2011-03-14 23:12:28 +0000

    * added sample for Bullet Physics integration for Wii and PC * cleaned svn ignore lists * cleaned pn project files

---
+++ Samples/Sample8/src/Sample8.h
@@ -0,0 +1,51 @@
+#pragma once
+#ifndef SAMPLE8_H
+#define SAMPLE8_H
+
+#include "WireApplication.h"
+#include "btBulletDynamicsCommon.h"
+
+using namespace Wire;
+
+class Sample8 : public WIREAPPLICATION
+{
+	WIRE_DECLARE_INITIALIZE;
+
+	typedef WIREAPPLICATION Parent;
+
+public:
+	Sample8();
+	virtual Bool OnInitialize();
+	virtual void OnIdle();
+	virtual void OnTerminate();
+
+private:
+	void CreatePhysics();
+	void UpdatePhysics();
+
+
+	inline btVector3 ToBtVector3(Vector3F in)
+	{
+		return btVector3(in.X(), in.Y(), in.Z());
+	}
+
+	inline void ToWireQuaternion(const btQuaternion& in, QuaternionF& out)
+	{
+		out = QuaternionF(in.getW(), in.getX(), in.getY(), in.getZ());
+	}
+
+	Culler mCuller;
+	TArray<GeometryPtr> mCubes;
+	CameraPtr mspCamera;
+
+	btDefaultCollisionConfiguration* mpCollisionConfiguration;
+	btCollisionDispatcher* mpDispatcher;
+	btAxisSweep3* mpOverlappingPairCache;
+	btSequentialImpulseConstraintSolver* mpSolver;
+	btDiscreteDynamicsWorld* mpDynamicsWorld;
+	btAlignedObjectArray<btCollisionShape*> mCollisionShapes;
+};
+
+WIRE_REGISTER_INITIALIZE(Sample8);
+
+#endif
