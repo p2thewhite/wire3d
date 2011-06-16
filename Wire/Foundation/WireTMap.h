7dbfe0f0ef4b7399c809eaec7b46475122264086
antibyte <antibyte@7d1a0020-0be9-11de-8884-7b8501395459>
2011-06-16 20:02:16 +0000

    * added (c) header comment to all files

--- Wire/Foundation/WireTMap.h
+++ Wire/Foundation/WireTMap.h
@@ -1,3 +1,11 @@
+// Wire3D by antibyte@gmail.com
+// http://wire3d.googlecode.com
+// Copyright(c) 2009-2011. All rights reserved.
+// 
+// The Wire3D source code is supplied under the terms of the LGPL and
+// may not be copied or disclosed except in accordance with the terms of
+// that agreement.
+
 #pragma once
 #ifndef WIRETMAP_H
 #define WIRETMAP_H
