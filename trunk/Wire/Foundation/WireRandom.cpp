7fda371ccf6fb31691c4b5546f08ce06f2fdecfc
antibyte <antibyte@7d1a0020-0be9-11de-8884-7b8501395459>
2011-06-16 20:02:16 +0000

    * added (c) header comment to all files

--- trunk/Wire/Foundation/WireRandom.cpp
+++ trunk/Wire/Foundation/WireRandom.cpp
@@ -9,6 +9,14 @@ Random::Random(UInt seed)
 {
 	Init(seed);
 }
+// Wire3D by antibyte@gmail.com
+// http://wire3d.googlecode.com
+// Copyright(c) 2009-2011. All rights reserved.
+// 
+// The Wire3D source code is supplied under the terms of the LGPL and
+// may not be copied or disclosed except in accordance with the terms of
+// that agreement.
+
 
 //----------------------------------------------------------------------------
 UInt Random::Get()
